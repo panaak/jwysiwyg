ee6a4472d97075bc03775e97fe77f1e33461f9c5
Nick Baugh <niftylettuce@gmail.com>
2013-11-01 13:49:23 -0400

    converted to paste event

--- src/jwysiwyg/private.js
+++ src/jwysiwyg/private.js
@@ -977,19 +977,14 @@ html: '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.o
 			}
 
 			if (self.options.plugins.rmFormat.rmMsWordMarkup) {
-				$(self.editorDoc).bind("keydown.wysiwyg", function (event) {
-					if (event.ctrlKey || event.metaKey) {
-						// CTRL + V (paste)
-						if (86 === event.keyCode) {
-							if ($.wysiwyg.rmFormat) {
-								if ("object" === typeof (self.options.plugins.rmFormat.rmMsWordMarkup)) {
-									$.wysiwyg.rmFormat.run(self, {rules: { msWordMarkup: self.options.plugins.rmFormat.rmMsWordMarkup }});
-								} else {
-									$.wysiwyg.rmFormat.run(self, {rules: { msWordMarkup: { enabled: true }}});
-								}
-							}
-						}
-					}
+				$(self.editorDoc).bind("paste.wysiwyg", function (event) {
+          if ($.wysiwyg.rmFormat) {
+            if ("object" === typeof (self.options.plugins.rmFormat.rmMsWordMarkup)) {
+              $.wysiwyg.rmFormat.run(self, {rules: { msWordMarkup: self.options.plugins.rmFormat.rmMsWordMarkup }});
+            } else {
+              $.wysiwyg.rmFormat.run(self, {rules: { msWordMarkup: { enabled: true }}});
+            }
+          }
 				});
 			}
 
